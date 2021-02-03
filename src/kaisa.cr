465ed7c36cf3f4ed2fe944166ce9dcc8a72607c8
Hannes Braun <hannesbraun@mail.de>
2021-02-04 00:24:09 +0100

    First draft

--- src/kaisa.cr
+++ src/kaisa.cr
@@ -1,5 +1,72 @@
+require "option_parser"
+require "socket"
+
 module Kaisa
   VERSION = "0.1.0"
 
-  # TODO: Put your CODE HERE
+  server, port = "localhost", 3100
+
+  option_parser = OptionParser.parse do |parser|
+    parser.banner = "Kaisa - A manual Spark agent"
+
+    parser.on "-v", "--version", "Show version" do
+      puts "version #{VERSION}"
+      exit
+    end
+    parser.on "-h", "--help", "Show help" do
+      puts parser
+      exit
+    end
+    parser.on "-s INSERVER", "--server INSERVER", "Server address" do |inserver|
+      server = inserver
+    end
+    parser.on "-p INPORT", "--port=INPORT", "Server port" do |inport|
+      port = inport
+    end
+    parser.missing_option do |option_flag|
+      STDERR.puts "ERROR: #{option_flag} is missing something."
+      STDERR.puts ""
+      STDERR.puts parser
+      exit(1)
+    end
+    parser.invalid_option do |option_flag|
+      STDERR.puts "ERROR: #{option_flag} is not a valid option."
+      STDERR.puts parser
+      exit(1)
+    end
+  end
+
+  puts "Connecting to #{server}:#{port}\n"
+
+  socket = TCPSocket.new(server, port)
+  # Effectors
+  spawn do
+    STDIN.blocking = true
+
+    loop do
+      msg = gets.not_nil!
+      break if msg == "exit"
+
+      if msg != nil
+        socket << msg.size
+        socket << msg
+      end
+    end
+
+    socket.close
+  end
+
+  # Perception
+  spawn do
+    while !(socket.closed?)
+      response = socket.gets
+      puts response
+    end
+  end
+
+  # Wait until socket is closed
+  loop do
+    Fiber.yield
+    break if socket.closed?
+  end
 end
